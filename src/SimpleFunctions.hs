9e58a3a91907ff5be770eb242c50ffd4db52dda8
Ivan <vana.shimko@gmail.com>
2016-07-07 11:00:06 +0300

    Refactoring

--- src/SimpleFunctions.hs
+++ src/SimpleFunctions.hs
@@ -3,27 +3,20 @@ module Chapter2 where
     firstOrEmpty lst = if not (null lst) then head lst else "empty"
 
     (+++) :: [t] -> [t] -> [t]
-    lst1 +++ lst2 = if null lst1
-                    then lst2
-                    else head lst1 : (tail lst1 +++ lst2)
+    [] +++ list2 = list2
+    (x:xs) +++ list2 = x:(xs +++ list2)
 
     reverse2 :: [t] -> [t]
-    reverse2 lst = if null lst
-                   then []
-                   else reverse (tail lst) +++ [head lst]
+    reverse2 [] = []
+    reverse2 (x:xs) = reverse2 xs ++ [x]
 
     maxmin :: (Ord t) => [t] -> (t, t)
-    maxmin list = let h = head list
-                  in if null (tail list)
-                     then (h, h)
-                     else ( if h > t_max then h else t_max
-                          , if h < t_min then h else t_min )
-                          where t = maxmin (tail list)
-                                t_max = fst t
-                                t_min = snd t
+    maxmin [x] = (x, x)
+    maxmin (x:xs) = ( if x > xs_max then x else xs_max
+                    , if x < xs_min then x else xs_min
+                    ) where (xs_max, xs_min) = maxmin xs
 
     fibonacci :: Integer -> Integer
-    fibonacci n = case n of
-                    0 -> 0
-                    1 -> 1
-                    _ -> fibonacci (n-1) + fibonacci (n-2)
+    fibonacci 0 = 0
+    fibonacci 1 = 1
+    fibonacci n = fibonacci (n-1) + fibonacci (n-2)
