e798752c40ecf13bc3566431e0736ae272307df7
vanashimko <vana.shimko@gmail.com>
2016-07-09 18:46:28 +0300

    indentation fix

--- src/SimpleFunctions.hs
+++ src/SimpleFunctions.hs
@@ -1,67 +1,68 @@
 module SimpleFunctions where
-    firstOrEmpty :: [[Char]] -> [Char]
-    firstOrEmpty lst = if not (null lst) then head lst else "empty"
 
-    (+++) :: [t] -> [t] -> [t]
-    [] +++ list2 = list2
-    (x:xs) +++ list2 = x:(xs +++ list2)
+firstOrEmpty :: [[Char]] -> [Char]
+firstOrEmpty lst = if not (null lst) then head lst else "empty"
 
-    reverse2 :: [t] -> [t]
-    reverse2 [] = []
-    reverse2 (x:xs) = reverse2 xs ++ [x]
+(+++) :: [t] -> [t] -> [t]
+[] +++ list2 = list2
+(x:xs) +++ list2 = x:(xs +++ list2)
 
-    maxmin :: (Ord t) => [t] -> (t, t)
-    maxmin [x] = (x, x)
-    maxmin (x:xs) = ( if x > xs_max then x else xs_max
-                    , if x < xs_min then x else xs_min
-                    ) where (xs_max, xs_min) = maxmin xs
+reverse2 :: [t] -> [t]
+reverse2 [] = []
+reverse2 (x:xs) = reverse2 xs ++ [x]
 
-    fibonacci :: Integer -> Integer
-    fibonacci 0 = 0
-    fibonacci 1 = 1
-    fibonacci n = fibonacci (n-1) + fibonacci (n-2)
+maxmin :: (Ord t) => [t] -> (t, t)
+maxmin [x] = (x, x)
+maxmin (x:xs) = ( if x > xs_max then x else xs_max
+                , if x < xs_min then x else xs_min
+                ) where (xs_max, xs_min) = maxmin xs
 
-    qsort :: (Ord a) => [a] -> [a]
-    qsort []     = []
-    qsort (x:xs) =  qsort lesser ++ [x] ++ qsort greater
-                       where   lesser = [a | a <- xs, a <= x]
-                               greater = [a | a <- xs, a > x]
+fibonacci :: Integer -> Integer
+fibonacci 0 = 0
+fibonacci 1 = 1
+fibonacci n = fibonacci (n-1) + fibonacci (n-2)
 
-    selectionSort :: (Ord a) => [a] -> [a]
-    selectionSort [] = []
-    selectionSort list = current ++ greater
-                            where   current = [x | x <- list, x == first]
-                                    greater = selectionSort [x | x <- list, x > first]
-                                    first = minimum list
+qsort :: (Ord a) => [a] -> [a]
+qsort []     = []
+qsort (x:xs) =  qsort lesser ++ [x] ++ qsort greater
+                   where   lesser = [a | a <- xs, a <= x]
+                           greater = [a | a <- xs, a > x]
 
-    unzip' :: [(a, b)] -> ([a], [b])
-    unzip' [] = ([], [])
-    unzip' (x:xs) = ( fst x : fst unzipped
-                    , snd x : snd unzipped
-                    ) where unzipped = unzip' xs
+selectionSort :: (Ord a) => [a] -> [a]
+selectionSort [] = []
+selectionSort list = current ++ greater
+                        where   current = [x | x <- list, x == first]
+                                greater = selectionSort [x | x <- list, x > first]
+                                first = minimum list
 
-    filterOnes :: [Integer] -> [Integer]
-    filterOnes = filter (== 1)
+unzip' :: [(a, b)] -> ([a], [b])
+unzip' [] = ([], [])
+unzip' (x:xs) = ( fst x : fst unzipped
+                , snd x : snd unzipped
+                ) where unzipped = unzip' xs
 
-    filterAtElement :: (Eq a) => a -> [a] -> [a]
-    filterAtElement element = filter (== element)
+filterOnes :: [Integer] -> [Integer]
+filterOnes = filter (== 1)
 
-    filterNot :: (a -> Bool) -> [a] -> [a]
-    filterNot f = filter (not . f)
+filterAtElement :: (Eq a) => a -> [a] -> [a]
+filterAtElement element = filter (== element)
 
-    product' :: (Num a, Foldable b) => b a -> a
-    product' = foldr (*) 1
+filterNot :: (a -> Bool) -> [a] -> [a]
+filterNot f = filter (not . f)
 
-    product'' :: (Num a) => [a] -> a
-    product'' [] = 1
-    product'' (x:xs) = x * product'' xs
+product' :: (Num a, Foldable b) => b a -> a
+product' = foldr (*) 1
 
-    all' :: Foldable a => a Bool -> Bool
-    all' = foldr (&&) True
+product'' :: (Num a) => [a] -> a
+product'' [] = 1
+product'' (x:xs) = x * product'' xs
 
-    all'' :: [Bool] -> Bool
-    all'' [] = True
-    all'' (x:xs) = x && all'' xs
+all' :: Foldable a => a Bool -> Bool
+all' = foldr (&&) True
 
-    minimumBy :: (Ord a, Foldable b) => (c -> a) -> b c -> c
-    minimumBy f = foldr1 (\x y -> if f x < f y then x else y)
+all'' :: [Bool] -> Bool
+all'' [] = True
+all'' (x:xs) = x && all'' xs
+
+minimumBy :: (Ord a, Foldable b) => (c -> a) -> b c -> c
+minimumBy f = foldr1 (\x y -> if f x < f y then x else y)
