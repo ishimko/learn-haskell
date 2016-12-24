5b62f6e1936805615bcb720a65070a000b24e745
Ivan <vana.shimko@gmail.com>
2016-12-24 21:37:29 +0300

    [k-means]: Add new centroid phase

--- src/KMeans.hs
+++ src/KMeans.hs
@@ -7,9 +7,13 @@ import qualified Data.Map as M
 
 class Ord v => Vector v where
     distance :: v -> v -> Double
+    centroid :: [v] -> v
 
 instance Vector (Double, Double) where
     distance (a, b) (c, d) = sqrt $ (c - a)**2 + (d - b)**2
+    centroid lst =  let (u, v) = foldr (\(a, b) (c, d) -> (a+c, b+d)) (0.0, 0.0) lst
+                        n = fromIntegral $ length lst
+                    in (u / n, v / n)
 
 class Vector v => Vectorizable e v where
     toVector :: e -> v
@@ -24,3 +28,6 @@ clusterAssignmentPhase centroids points =
                                                             centroids
                             in M.adjust (p:) chosenCentroid m)
                 initialMap points
+
+newCentroidPhase :: (Vector v, Vectorizable e v) => M.Map v [e] -> [(v, v)]
+newCentroidPhase = M.toList . fmap (centroid . map toVector)
