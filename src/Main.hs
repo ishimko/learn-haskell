afc198132123d30f46c85ca182e2913144e8eedf
Ivan <vana.shimko@gmail.com>
2016-12-24 23:20:09 +0300

    [Main]: Add example printing

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,12 @@
 module Main (main) where
 
+import KMeans
+
+example :: ([(Double, Double)], Int)
+example = kMeans initializeSimple 2 ([(1, 1), (1,2), (4, 4), (4, 5)]::[(Double, Double)]) 0.001
+
+showResult ::  ([(Double, Double)], Int) -> String
+showResult (list, iterations) =  "Centroids: " ++ show list ++ " found with " ++ show iterations ++ " iterations"
 
 main :: IO ()
-main = return ()
+main = putStrLn $ showResult example
